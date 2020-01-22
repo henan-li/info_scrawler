85ac52ede942cdb29f04088174427aacb34e6b82
nathan <nathanli1994@hotmail.com>
2020-01-22 18:03:27 -0500

    get deeper page info

--- utils/colly.go
+++ utils/colly.go
@@ -6,13 +6,19 @@ import (
 	"github.com/gocolly/colly"
 	"log"
 	"os"
+	"strconv"
+	"time"
 )
 
-var(
+var (
 	baseUrl = "http://www.szlawyers.com"
+	number  = 1
+	c *colly.Collector
+	writer *csv.Writer
 )
 
 func DoWork(query string) {
+
 	fName := "lawFirmDetails.csv"
 	file, err := os.Create(fName)
 	if err != nil {
@@ -20,29 +26,88 @@ func DoWork(query string) {
 		return
 	}
 	defer file.Close()
-	writer := csv.NewWriter(file)
+	writer = csv.NewWriter(file)
 	defer writer.Flush()
 
-	// Write CSV header
-	writer.Write([]string{"律所名称", "设立时间", "负责人", "律师人数", "联系电话", "详情链接"})
+	writer.Write([]string{"", "律所名称", "设立时间", "负责人", "律师人数", "联系电话", "详情链接","办公地址"})
+
+
 
 	// Instantiate default collector
-	c := colly.NewCollector()
-
-	c.OnHTML("tbody tr:not(:first-child, :last-child)", func(e *colly.HTMLElement) {
-
-		// 流程: (注意:他是一次一个的去找,因此每一个row就对应页面上的一个row) 从tbody tr下面找td,然后将他的文本内容都取出来
-		// 然后在往当前row中动态追加一个url
-		// 最后写入csv,然后开始下一轮查找
-		row := e.ChildTexts("td")
-		link := e.Request.AbsoluteURL(e.ChildAttr("a", "href"))
-		row = append(row,link)
-		writer.Write(row)
+	c = colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
+		colly.Async(true),
+	)
+	c.Limit(&colly.LimitRule{
+		Parallelism: 5,
+		RandomDelay: 1 * time.Second,
 	})
 
-	url := baseUrl+query
-	fmt.Println(url)
-	c.Visit(url)
+	//c.OnRequest(func(r *colly.Request) {
+	//	fmt.Println("Visiting", r.URL.String())
+	//})
 
+	//c.OnResponse(func(r *colly.Response) {
+	//	fmt.Println("response with code: ", r.StatusCode)
+	//})
+
+	// main page
+	c.OnHTML(".tab_list tr:not(:first-child, :last-child)", func(e *colly.HTMLElement) {
+
+		row := e.ChildTexts("td")
+		link := e.Request.AbsoluteURL(e.ChildAttr("a", "href"))
+		row = append([]string{strconv.Itoa(number)}, row[0:]...)
+		row = append(row, link)
+
+		err := writer.Write(row)
+		if err != nil{
+			fmt.Println("信息写入错误！！！程序退出运行")
+			os.Exit(1)
+		}
+		number += 1
+	})
+
+	// go next
+	c.OnHTML(".page a:last-child", func(e *colly.HTMLElement) {
+
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		text := e.DOM.Text()
+		if text == "下一页"{
+			c.Visit(link)
+		}
+	})
+
+	// member detail page
+	//c.OnHTML(".list table[style*=\"word-break\"] tbody tr:nth-child(4), .list table[style*=\"word-break\"] tbody tr:nth-child(5), .list table[style*=\"word-break\"] tbody tr:nth-child(11)", func(e *colly.HTMLElement) {
+	//
+	//	personalDetail = e.ChildTexts("td")
+	//	personalDetail = personalDetail[1:]
+	//	fmt.Println(personalDetail)
+	//})
+
+	url := baseUrl + query
+	c.Visit(url)
+	c.Wait()
 	log.Printf("Scraping finished, check file %q for results\n", fName)
 }
+
+//func gotoCompanyDetails(row []string,link string) {
+//
+//	//link := strings.Join(row[6:],"")
+//	//c.Visit(link)
+//	b := colly.NewCollector()
+//	b.OnHTML(".list tbody tr:nth-child(12)", func(e *colly.HTMLElement) {
+//		location := e.ChildTexts("td")
+//		location = location[1:]
+//		row = append(row,location...)
+//
+//		err := writer.Write(row)
+//		if err != nil{
+//			fmt.Println("信息写入错误！！！程序退出运行")
+//			os.Exit(1)
+//		}
+//		//link := e.Request.AbsoluteURL(e.ChildAttr(".list tbody tr:nth-child(11) span a","href"))
+//	})
+//
+//	b.Visit(link)
+//}
